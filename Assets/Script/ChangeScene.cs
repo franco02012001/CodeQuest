663b8a11c684f7b77f946e1f5e389831562bc4f0
franco02012001 <fraco.enguerra@gmail.com>
2024-04-09 01:42:01 +0800

    add scene functions
    connecting scene

--- Assets/Script/ChangeScene.cs
+++ Assets/Script/ChangeScene.cs
@@ -3,9 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void GoToOtherScene()
+    public void GoToUserName()
     {
-        //This displays a Button on the screen at position (20,30), width 150 and height 50. The button’s text reads the last parameter. Press this for the SceneManager to load the Scene.
+        
         SceneManager.LoadScene("UserName");
     }
+
+    public void GoToMainpage()
+    {
+        
+        SceneManager.LoadScene("Mainpage");
+    }
+    public void GoToSever()
+    {
+        
+        SceneManager.LoadScene("Server");
+    }
+    public void GoToLandingPage()
+    {
+
+        SceneManager.LoadScene("LandingPage");
+    }
+
+
 }
